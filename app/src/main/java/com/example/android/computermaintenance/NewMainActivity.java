257cde653947a387d34b3bc59e7694241a3545c0
Raza Saleemi <rks143@gmail.com>
2017-11-14 22:05:24 +0500

    Two_Activities

---
+++ app/src/main/java/com/example/android/computermaintenance/NewMainActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.computermaintenance;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+
+public class NewMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_main);
+    }
+
+    /** Called when the user taps the Register button */
+    public void registerUser(View view) {
+        // Do something in response to button
+        // Main Activity is handling Registration Process
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        startActivity(intent);
+    }
+}
